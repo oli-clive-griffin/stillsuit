0c4f66f4742932f7fce00173205cc5b9c923c543
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2021-07-01 01:28:52 +1200

    get friendship object

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from time import sleep
 import tweepy as tw
 from dotenv import load_dotenv
 import os 
@@ -11,22 +10,21 @@ CONSUMER_SECRET = os.environ.get("API_SECRET_KEY")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
-# print(CONSUMER_KEY)
-# print(CONSUMER_SECRET)
-# print(ACCESS_TOKEN)
-# print(ACCESS_TOKEN_SECRET)
-
 auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tw.API(auth)
-friends = api.friends("oli_c_g")
 
-print(friends[0].name)
+
+print(api.rate_limit_status())
+
+me = api.me()
+
+friends = api.friends(me.name)
+
 latest_follow = friends[0]
 
-api.destroy_friendship(latest_follow.id)
+# api.destroy_friendship(latest_follow.id)
 
-friendships = api.lookup_friendships([latest_follow.id, friends[7].id])
-
-print(friendships, file=open("friendships.txt", "w"))
+print(latest_follow.id)
+print(me.id)
